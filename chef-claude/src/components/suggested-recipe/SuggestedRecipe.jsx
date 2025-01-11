4f5e9fdcdc21d1923fd2a95a920da8b271c720bf
Ahmed Marzook <m4marzook@gmail.com>
2025-01-11 20:38:51 +0000

    Formatted Recipe using React Markdown

--- chef-claude/src/components/suggested-recipe/SuggestedRecipe.jsx
+++ chef-claude/src/components/suggested-recipe/SuggestedRecipe.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./SuggestedRecipe.css";
+import ReactMarkdown from "react-markdown";
 
 function SuggestedRecipe(props) {
   console.log(props.recipe);
   return (
     <div className="suggested-recipe-container">
       <h2>Chef Claude Recommends:</h2>
-      {props.recipe}
+      <ReactMarkdown>{props.recipe}</ReactMarkdown>
     </div>
   );
 }
